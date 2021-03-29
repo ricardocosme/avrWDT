b84e22f4ce59c19ef3545995afc51dc88dbbb5b8
Ricardo Cosme <ricardo@cosme.im>
2021-03-29 19:43:28 -0300

    Fix usage of requires(is_timeout<Timeout>)

--- include/avr/wdt/timeout.hpp
+++ include/avr/wdt/timeout.hpp
@@ -2,8 +2,8 @@
 
 #include "avr/wdt/timeout/common.hpp"
 
-#if __cplusplus >= 201703L
-#define AVR_WDT_REQUIRES_TIMEOUT requires(is_timeout<Timeout>::value)
+#if __cplusplus > 201703L
+#define AVR_WDT_REQUIRES_TIMEOUT requires(avr::wdt::is_timeout<Timeout>::value)
 #else
 #define AVR_WDT_REQUIRES_TIMEOUT
 #endif
